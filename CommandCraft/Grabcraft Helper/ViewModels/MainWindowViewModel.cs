26fabccaccbadd436352a2ebe9f1b9045f5f0326
r i v a e l <rivael256@gmail.com>
2019-08-21 21:34:34 +0200

    Save before changes

---
+++ CommandCraft/Grabcraft Helper/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grabcraft_Helper.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
